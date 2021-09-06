12ffbfdb69db2bc08a86695406498725f7b2c53e
Tan-DESKPC <tan@tanho.ca>
2021-09-06 18:41:03 -0400

    fix summarise for inseason

--- R/8_summaries.R
+++ R/8_summaries.R
@@ -50,8 +50,9 @@ ffs_summarise_week <- function(optimal_scores, schedules) {
     by = c("season","week")
   ][,`:=`(allplay_pct = round(allplay_wins / allplay_games, 3))]
 
+  team <- data.table::copy(optimal_scores)
   opponent <- data.table::copy(optimal_scores)
-  data.table::setnames(optimal_scores,old = "actual_score",new = "team_score")
+  data.table::setnames(team,old = "actual_score",new = "team_score")
   data.table::setnames(opponent,
                        old = c("actual_score","franchise_id","franchise_name"),
                        new = c("opponent_score","opponent_id","opponent_name"))
@@ -64,7 +65,7 @@ ffs_summarise_week <- function(optimal_scores, schedules) {
   lineup_efficiency <- NULL
 
   summary_week <- schedules[
-    optimal_scores, on = c("league_id","franchise_id", "season", "week")
+    team, on = c("league_id","franchise_id", "season", "week")
   ][opponent, on = c("league_id","opponent_id", "season", "week")
   ][,`:=`(result = data.table::fcase(team_score > opponent_score, "W",
                                      team_score < opponent_score, "L",
@@ -169,6 +170,51 @@ ffs_summarise_simulation <- function(summary_season) {
 }
 
 ffs_mean <- function(...){round(mean(...,na.rm = TRUE),3)}
+
+#' Summarise Inseason Simulation
+#'
+#' @param summary_week a dataframe as created by `ffs_summarise_week()`
+#' @param n number of weeks
+#' @rdname ffs_summaries
+#' @return ffs_summarise_inseason: a dataframe summarising franchise results for the inseason simulation
+#' @export
+ffs_summarise_inseason <- function(summary_week,n) {
+  checkmate::assert_number(n)
+  checkmate::assert_data_frame(summary_week)
+  assert_columns(
+    summary_week,
+    c(
+      "league_id", "franchise_id", "franchise_name",
+      "result", "allplay_wins", "allplay_games",
+      "team_score", "opponent_score", "optimal_score"
+    )
+  )
+  data.table::setDT(summary_week)
+
+  result <- NULL
+  allplay_wins <- NULL
+  allplay_games <- NULL
+  team_score <- NULL
+  opponent_score <- NULL
+  optimal_score <- NULL
+
+  summary_season <- summary_week[
+    ,
+    .(h2h_wins = sum(result == "W", na.rm = TRUE),
+      h2h_winpct = round(sum(result == "W", na.rm = TRUE) / .N,3),
+      allplay_wins = sum(allplay_wins, na.rm = TRUE),
+      allplay_games = sum(allplay_games, na.rm = TRUE),
+      allplay_winpct = round(sum(allplay_wins, na.rm = TRUE) / sum(allplay_games, na.rm = TRUE),3),
+      points_for = round(sum(team_score, na.rm = TRUE)/n,2),
+      points_against = round(sum(opponent_score, na.rm = TRUE)/n,2),
+      potential_points = round(sum(optimal_score, na.rm = TRUE)/n,2)
+    ),
+    by = c("league_id","franchise_id","franchise_name")
+  ]
+
+  return(summary_season)
+}
+
 
 #' @rdname ffs_summaries
 #' @export
