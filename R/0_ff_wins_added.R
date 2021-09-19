e2bf8359fcbf6c91afc3c1aca3728b493a819c89
Tan-DESKPC <tan@tanho.ca>
2021-09-18 20:59:13 -0400

    start of wins-added

---
+++ R/0_ff_wins_added.R
@@ -0,0 +1,32 @@
+#' Wins Added
+
+#' Run base simulation once for n seasons/weeks etc
+#'
+#' For every player on every team (except replacement level players),
+#'
+#' - remove them from that specific roster
+#' - reoptimize the lineups just for that roster without the player
+#' - use that set of weekly scores as the score for that week
+#' - summarise the new simulation
+#' - return new wins total and points total for that franchise
+#'
+#' Summarise WA as the difference between the sim with the player and the sim without them
+
+ff_wins_added <- function(){
+
+  #' Run base simulation once for n seasons/weeks etc
+
+  #' For every player on every team (except replacement level players),
+  #'
+  #' - remove them from that specific roster
+  #' - reoptimize the lineups just for that roster without the player
+  #' - use that set of weekly scores as the score for that week
+  #' - summarise the new simulation
+  #' - return new wins total and points total for that franchise
+  #'
+  #' Summarise WA as the difference between the sim with the player and the sim without them
+
+
+
+}
+
